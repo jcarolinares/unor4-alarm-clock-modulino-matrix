228ca87648ff400d210e2971552c9b365493fa4b
jcarolinares <jcarolinares@gmail.com>
2026-04-09 18:31:26 +0200

    Initial Commit

---
+++ unor4-alarm-clock-modulino-matrix/matrix_collection.h
@@ -0,0 +1,190 @@
+// UNO R4 Matrix frames
+
+const uint32_t heart[] = {
+    0x3184a444,
+    0x44042081,
+    0x100a0040
+};
+
+constexpr uint32_t frame_MON[] = {
+  0x000aeeea,
+  0xaaaaaaaa,
+  0xea000000
+};
+
+constexpr uint32_t frame_TUE[] = {
+  0x000eae4a,
+  0x84ac4a84,
+  0xee000000
+};
+
+constexpr uint32_t frame_WED[] = {
+  0x000aeca8,
+  0xaacae8aa,
+  0xec000000
+};
+
+constexpr uint32_t frame_THU[] = {
+  0x000eaa4a,
+  0xa4ea4aa4,
+  0xae000000
+};
+
+constexpr uint32_t frame_FRI[] = {
+  0x000eee8a,
+  0x4cc48a48,
+  0xae000000
+};
+
+constexpr uint32_t frame_SAT[] = {
+  0x000eee8a,
+  0x4eee2a4e,
+  0xa4000000
+};
+
+constexpr uint32_t frame_SUN[] = {
+  0x000eaa8a,
+  0xeeaa2aae,
+  0xea000000
+};
+
+// 2:45 Clock face + "H" (Hour)
+constexpr uint32_t icon_HOUR[] = {
+  0x38044a8a,
+  0xaf2e82a4,
+  0x4a380000
+};
+
+// 2:45 Clock face + "M" (Minute)
+constexpr uint32_t icon_MIN[] = {
+  0x38044a8a,
+  0xef2a82a4,
+  0x4a380000
+};
+
+// END of UNO R4 Matrix frames
+
+// constexpr const uint32_t* matrixIcon[] = {
+//   LEDMATRIX_BLUETOOTH,
+//   LEDMATRIX_BOOTLOADER_ON,
+//   LEDMATRIX_CHIP,
+//   LEDMATRIX_CLOUD_WIFI,
+//   LEDMATRIX_DANGER,
+//   LEDMATRIX_EMOJI_BASIC,
+//   LEDMATRIX_EMOJI_HAPPY,
+//   LEDMATRIX_EMOJI_SAD,
+//   LEDMATRIX_HEART_BIG,
+//   LEDMATRIX_HEART_SMALL,
+//   LEDMATRIX_LIKE,
+//   LEDMATRIX_MUSIC_NOTE,
+//   LEDMATRIX_RESISTOR,
+//   LEDMATRIX_UNO
+// };
+
+// constexpr const uint32_t (*matrixAnimation[])[4] = {
+//   LEDMATRIX_ANIMATION_STARTUP,
+//   LEDMATRIX_ANIMATION_TETRIS_INTRO,
+//   LEDMATRIX_ANIMATION_ATMEGA,
+//   LEDMATRIX_ANIMATION_LED_BLINK_HORIZONTAL,
+//   LEDMATRIX_ANIMATION_LED_BLINK_VERTICAL,
+//   LEDMATRIX_ANIMATION_ARROWS_COMPASS,
+//   LEDMATRIX_ANIMATION_AUDIO_WAVEFORM,
+//   LEDMATRIX_ANIMATION_BATTERY,
+//   LEDMATRIX_ANIMATION_BOUNCING_BALL,
+//   LEDMATRIX_ANIMATION_BUG,
+//   LEDMATRIX_ANIMATION_CHECK,
+//   LEDMATRIX_ANIMATION_CLOUD,
+//   LEDMATRIX_ANIMATION_DOWNLOAD,
+//   LEDMATRIX_ANIMATION_DVD,
+//   LEDMATRIX_ANIMATION_HEARTBEAT_LINE,
+//   LEDMATRIX_ANIMATION_HEARTBEAT,
+//   LEDMATRIX_ANIMATION_INFINITY_LOOP_LOADER,
+//   LEDMATRIX_ANIMATION_LOAD_CLOCK,
+//   LEDMATRIX_ANIMATION_LOAD,
+//   LEDMATRIX_ANIMATION_LOCK,
+//   LEDMATRIX_ANIMATION_NOTIFICATION,
+//   LEDMATRIX_ANIMATION_OPENSOURCE,
+//   LEDMATRIX_ANIMATION_SPINNING_COIN,
+//   LEDMATRIX_ANIMATION_TETRIS,
+//   LEDMATRIX_ANIMATION_WIFI_SEARCH,
+//   LEDMATRIX_ANIMATION_HOURGLASS
+// };
+
+
+constexpr const char* const matrixText[] = {
+  "   VIETNAM ARDUINO DAYS   ",
+  "   THIS IS THE NEW MODULINO MATRIX   ",
+  "   Xin chao!!!   "
+};
+
+
+const uint32_t number_0[5] = {
+		0x3fc40,
+		0x24024024,
+		0x23fc000,
+		66,
+    100
+	};
+  
+const uint8_t bottle_frame [] = {
+  0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0,
+  0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0
+};
+
+
+// Numbers 0 to 9 in 3x32-bit format (for vertical 12x8 layout)
+constexpr uint32_t NUM_0[3] = { 0x00182424, 0x24242424, 0x24180000 };
+constexpr uint32_t NUM_1[3] = { 0x00081828, 0x08080808, 0x083C0000 };
+constexpr uint32_t NUM_2[3] = { 0x00182404, 0x04081020, 0x203C0000 };
+constexpr uint32_t NUM_3[3] = { 0x00182404, 0x08180404, 0x24180000 };
+constexpr uint32_t NUM_4[3] = { 0x00081828, 0x487C0808, 0x08080000 };
+constexpr uint32_t NUM_5[3] = { 0x003C2020, 0x38040404, 0x24180000 };
+constexpr uint32_t NUM_6[3] = { 0x00182420, 0x38242424, 0x24180000 };
+constexpr uint32_t NUM_7[3] = { 0x003C0404, 0x08081010, 0x20200000 };
+constexpr uint32_t NUM_8[3] = { 0x00182424, 0x18242424, 0x24180000 };
+constexpr uint32_t NUM_9[3] = { 0x00182424, 0x241C0404, 0x24180000 };
+
+// BIG Numbers 0 to 9 in 3x32-bit format (Full 12x8 vertical utilization)
+constexpr uint32_t BIG_NUM_0[3] = { 0x3C666666, 0x66666666, 0x6666663C };
+constexpr uint32_t BIG_NUM_1[3] = { 0x18387818, 0x18181818, 0x1818187E };
+constexpr uint32_t BIG_NUM_2[3] = { 0x3C666606, 0x0C183060, 0x60607E7E };
+constexpr uint32_t BIG_NUM_3[3] = { 0x3C660606, 0x3E3E0606, 0x0666663C };
+constexpr uint32_t BIG_NUM_4[3] = { 0x0C1C3C6C, 0x6C6C7E7E, 0x0C0C0C0C };
+constexpr uint32_t BIG_NUM_5[3] = { 0x7E7E6060, 0x7C7E0606, 0x0666663C };
+constexpr uint32_t BIG_NUM_6[3] = { 0x3C666060, 0x7C7E6666, 0x6666663C };
+constexpr uint32_t BIG_NUM_7[3] = { 0x7E7E0606, 0x0C0C1818, 0x30303030 };
+constexpr uint32_t BIG_NUM_8[3] = { 0x3C666666, 0x3C3C6666, 0x6666663C };
+constexpr uint32_t BIG_NUM_9[3] = { 0x3C666666, 0x667E3E06, 0x0606663C };
+
+// 7-Segment Style Numbers 0 to 9 in 3x32-bit format (12x8 vertical layout)
+constexpr uint32_t SEG_NUM_0[3] = { 0x7E7E6666, 0x66666666, 0x66667E7E };
+constexpr uint32_t SEG_NUM_1[3] = { 0x06060606, 0x06060606, 0x06060606 };
+constexpr uint32_t SEG_NUM_2[3] = { 0x7E7E0606, 0x067E7E60, 0x60607E7E };
+constexpr uint32_t SEG_NUM_3[3] = { 0x7E7E0606, 0x067E7E06, 0x06067E7E };
+constexpr uint32_t SEG_NUM_4[3] = { 0x66666666, 0x667E7E06, 0x06060606 };
+constexpr uint32_t SEG_NUM_5[3] = { 0x7E7E6060, 0x607E7E06, 0x06067E7E };
+constexpr uint32_t SEG_NUM_6[3] = { 0x7E7E6060, 0x607E7E66, 0x66667E7E };
+constexpr uint32_t SEG_NUM_7[3] = { 0x7E7E0606, 0x06060606, 0x06060606 };
+constexpr uint32_t SEG_NUM_8[3] = { 0x7E7E6666, 0x667E7E66, 0x66667E7E };
+constexpr uint32_t SEG_NUM_9[3] = { 0x7E7E6666, 0x667E7E06, 0x06067E7E };
+
+// 7-Segment Numbers (4px wide) WITH a colon on the right side
+// 7-Segment Numbers (6px wide) WITH a narrow 1px colon on the right edge
+// Format: 6 columns for the digit, 1 empty column, 1 column for the colon
+
+constexpr uint32_t COLON_NUM_0[3] = { 0xFCFCCCCD, 0xCDCCCCCD, 0xCDCCFCFC };
+constexpr uint32_t COLON_NUM_1[3] = { 0x0C0C0C0D, 0x0D0C0C0D, 0x0D0C0C0C };
+constexpr uint32_t COLON_NUM_2[3] = { 0xFCFC0C0D, 0x0DFCFCC1, 0xC1C0FCFC };
+constexpr uint32_t COLON_NUM_3[3] = { 0xFCFC0C0D, 0x0DFCFC0D, 0x0D0CFCFC };
+constexpr uint32_t COLON_NUM_4[3] = { 0xCCCCCCCD, 0xCDFCFC0D, 0x0D0C0C0C };
+constexpr uint32_t COLON_NUM_5[3] = { 0xFCFCC0C1, 0xC1FCFC0D, 0x0D0CFCFC };
+constexpr uint32_t COLON_NUM_6[3] = { 0xFCFCC0C1, 0xC1FCFCCD, 0xCDCCFCFC };
+constexpr uint32_t COLON_NUM_7[3] = { 0xFCFC0C0D, 0x0D0C0C0D, 0x0D0C0C0C };
+constexpr uint32_t COLON_NUM_8[3] = { 0xFCFCCCCD, 0xCDFCFCCD, 0xCDCCFCFC };
+constexpr uint32_t COLON_NUM_9[3] = { 0xFCFCCCCD, 0xCDFCFC0D, 0x0D0CFCFC };
---
+++ unor4-alarm-clock-modulino-matrix/unor4-alarm-clock-modulino-matrix.ino
@@ -0,0 +1,504 @@
+/*
+ Arduino matrix_0 LED Nixie Clock
+
+ A Nixie clock made with four Modulinos LED matrix_0
+
+ Made by Julián Caro Linares for Arduino INC
+
+ CC-BY-SA
+*/
+
+#include <Arduino_Modulino.h>
+#include "ArduinoGraphics.h"
+#include "Modulino_LED_Matrix.h"
+// #include "LEDMatrixGallery.h" // This header contains predefined animations for the LED matrix_0 display.
+#include "matrix_collection.h"
+#include "RTC.h"
+#include "Arduino_LED_Matrix.h" // Library for the Integrated Matrix of the UNO R4/UNOQ
+
+// RTC and NTP Synch libraries
+#include "RTC.h"
+#include <NTPClient.h>
+
+// Wifi Libraries
+#include <WiFiS3.h>
+#include <WiFiUdp.h>
+// #include "arduino_secrets.h" 
+
+// Wifi and NTP Objects
+int wifiStatus = WL_IDLE_STATUS;
+WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+NTPClient timeClient(Udp);
+
+// Modulino Matrix Objects
+ModulinoLEDMatrix matrix_0;
+ModulinoLEDMatrix matrix_1(0x40);
+ModulinoLEDMatrix matrix_2(0x41);
+ModulinoLEDMatrix matrix_3(0x42);
+
+// Modulino Objects
+ModulinoKnob knob;
+ModulinoBuzzer buzzer;
+
+// Arduino Matrix
+ArduinoLEDMatrix integrated_matrix;
+
+// Modulino knob variables
+int position = 0; 
+bool click = false;
+int8_t direction = 0;
+
+// Modulino buzzer variables
+int frequency = 440;  // Frequency of the tone in Hz
+int duration = 100;  // Duration of the tone in milliseconds
+
+
+// RTC Variables
+// RTCTime currentTime;
+int hours = 0;
+int minutes = 0;
+// DayOfWeek day_of_week = DayOfWeek::THURSDAY;
+
+// Initial time
+RTCTime currentTime(7, Month::APRIL, 2026, 0, 0, 00, DayOfWeek::MONDAY, SaveLight::SAVING_TIME_ACTIVE);
+
+// Alarm time & Variables
+RTCTime alarmTime(7, Month::APRIL, 2026, 17, 30, 00, DayOfWeek::MONDAY, SaveLight::SAVING_TIME_ACTIVE);
+bool alarm_status = true;
+
+void setup() {
+
+  Serial.begin(115200);
+  RTC.begin();
+
+  // Modulinos Begin
+  Modulino.begin();
+  knob.begin();
+  buzzer.begin();
+
+  // Matrix_ Init Setup
+  matrix_0.begin();
+  matrix_1.begin();
+  matrix_2.begin();
+  matrix_3.begin();
+
+  // Integrated Matrix
+  integrated_matrix.begin();
+
+  // Time and Date Init Setup
+  currentTime = RTCTime(7, Month::APRIL, 2026, hours, minutes, 00, DayOfWeek::TUESDAY, SaveLight::SAVING_TIME_ACTIVE);
+  RTC.setTime(currentTime);
+  matrix_show_time();
+
+  // integrated_matrix_date();
+  set_time();
+  
+}
+
+void loop() {
+
+  // Get current time from RTC
+  RTC.getTime(currentTime);
+
+  matrix_show_time();
+  integrated_matrix_date();
+  check_alarm();
+  delay(10000); // FIXME change this for a millis thing
+
+}
+
+
+void integrated_matrix_date()
+{
+
+  DayOfWeek day_of_week = currentTime.getDayOfWeek();
+  if(day_of_week == DayOfWeek::MONDAY){
+    integrated_matrix.loadFrame(frame_MON);
+  }
+  else if(day_of_week == DayOfWeek::TUESDAY){
+    integrated_matrix.loadFrame(frame_TUE);
+  }
+  else if(day_of_week == DayOfWeek::WEDNESDAY){
+    integrated_matrix.loadFrame(frame_WED);
+  }
+  else if(day_of_week == DayOfWeek::THURSDAY){
+    integrated_matrix.loadFrame(frame_THU);
+  }
+  else if(day_of_week == DayOfWeek::FRIDAY){
+    integrated_matrix.loadFrame(frame_FRI);
+  }
+  else if(day_of_week == DayOfWeek::SATURDAY){
+    integrated_matrix.loadFrame(frame_SAT);
+  }
+  else if(day_of_week == DayOfWeek::SUNDAY){
+    integrated_matrix.loadFrame(frame_SUN);
+  }
+  delay(100);
+}
+
+void check_alarm(){
+  int current_hour = currentTime.getHour();
+  int alarm_hour = alarmTime.getHour();
+  int current_minutes = currentTime.getMinutes();
+  int alarm_minutes = alarmTime.getMinutes();
+
+  // Alarm is ON
+  if (alarm_status == true && current_hour == alarm_hour && current_minutes == alarm_minutes ){
+    for(int i = 0; i<20; i++){
+      buzzer.tone(494, 500);
+      delay(250);
+      buzzer.tone(0, 500);
+      delay(250);
+    }
+    alarm_status = false;
+  }
+}
+
+void matrix_show_time()
+{
+
+  int hour_tens = currentTime.getHour()/10;
+  int hour_units = currentTime.getHour()%10;
+  int minutes_tens = currentTime.getMinutes()/10;
+  int minutes_units = currentTime.getMinutes()%10;
+
+  switch (hour_tens){
+    case 0:
+      matrix_3.setFrame(SEG_NUM_0);
+      break;
+    case 1:
+      matrix_3.setFrame(SEG_NUM_1);
+      break;
+    case 2:
+      matrix_3.setFrame(SEG_NUM_2);
+      break;
+    case 3:
+      matrix_3.setFrame(SEG_NUM_3);
+      break;
+    case 4:
+      matrix_3.setFrame(SEG_NUM_4);
+      break;
+    case 5:
+      matrix_3.setFrame(SEG_NUM_5);
+      break;
+    case 6:
+      matrix_3.setFrame(SEG_NUM_6);
+      break;
+    case 7:
+      matrix_3.setFrame(SEG_NUM_7);
+      break;
+    case 8:
+      matrix_3.setFrame(SEG_NUM_8);
+      break;
+    case 9:
+      matrix_3.setFrame(SEG_NUM_9);
+      break;
+  }
+
+  switch (hour_units){
+    case 0:
+      matrix_2.setFrame(COLON_NUM_0);
+      break;
+    case 1:
+      matrix_2.setFrame(COLON_NUM_1);
+      break;
+    case 2:
+      matrix_2.setFrame(COLON_NUM_2);
+      break;
+    case 3:
+      matrix_2.setFrame(COLON_NUM_3);
+      break;
+    case 4:
+      matrix_2.setFrame(COLON_NUM_4);
+      break;
+    case 5:
+      matrix_2.setFrame(COLON_NUM_5);
+      break;
+    case 6:
+      matrix_2.setFrame(COLON_NUM_6);
+      break;
+    case 7:
+      matrix_2.setFrame(COLON_NUM_7);
+      break;
+    case 8:
+      matrix_2.setFrame(COLON_NUM_8);
+      break;
+    case 9:
+      matrix_2.setFrame(COLON_NUM_9);
+      break;
+  }
+
+  switch (minutes_tens){
+    case 0:
+      matrix_1.setFrame(SEG_NUM_0);
+      break;
+    case 1:
+      matrix_1.setFrame(SEG_NUM_1);
+      break;
+    case 2:
+      matrix_1.setFrame(SEG_NUM_2);
+      break;
+    case 3:
+      matrix_1.setFrame(SEG_NUM_3);
+      break;
+    case 4:
+      matrix_1.setFrame(SEG_NUM_4);
+      break;
+    case 5:
+      matrix_1.setFrame(SEG_NUM_5);
+      break;
+    case 6:
+      matrix_1.setFrame(SEG_NUM_6);
+      break;
+    case 7:
+      matrix_1.setFrame(SEG_NUM_7);
+      break;
+    case 8:
+      matrix_1.setFrame(SEG_NUM_8);
+      break;
+    case 9:
+      matrix_1.setFrame(SEG_NUM_9);
+      break;
+  }
+
+  switch (minutes_units){
+    case 0:
+      matrix_0.setFrame(SEG_NUM_0);
+      break;
+    case 1:
+      matrix_0.setFrame(SEG_NUM_1);
+      break;
+    case 2:
+      matrix_0.setFrame(SEG_NUM_2);
+      break;
+    case 3:
+      matrix_0.setFrame(SEG_NUM_3);
+      break;
+    case 4:
+      matrix_0.setFrame(SEG_NUM_4);
+      break;
+    case 5:
+      matrix_0.setFrame(SEG_NUM_5);
+      break;
+    case 6:
+      matrix_0.setFrame(SEG_NUM_6);
+      break;
+    case 7:
+      matrix_0.setFrame(SEG_NUM_7);
+      break;
+    case 8:
+      matrix_0.setFrame(SEG_NUM_8);
+      break;
+    case 9:
+      matrix_0.setFrame(SEG_NUM_9);
+      break;
+  }
+
+}
+
+
+void set_time(){
+
+  position = knob.get();
+  click = knob.isPressed();
+  direction = knob.getDirection();
+
+  // Hours tens
+  while(click == 0)
+  {
+    integrated_matrix.loadFrame(icon_HOUR);
+    // Serial.println(hours);
+    if (direction == 1 && hours < 24){
+      buzzer.tone(330, duration);
+      hours++;
+      currentTime = RTCTime(7, Month::APRIL, 2026, hours, minutes, 00, DayOfWeek::MONDAY, SaveLight::SAVING_TIME_ACTIVE);
+      RTC.setTime(currentTime);
+      matrix_show_time();
+    }
+    else if(direction == -1 && hours > 0){
+      hours--;
+      currentTime = RTCTime(7, Month::APRIL, 2026, hours, minutes, 00, DayOfWeek::MONDAY, SaveLight::SAVING_TIME_ACTIVE);
+      RTC.setTime(currentTime);
+      matrix_show_time();
+    }
+
+    position = knob.get();
+    click = knob.isPressed();
+    direction = knob.getDirection();
+  }
+
+  // Serial.println("Hours Set");
+  delay(1000); // Delay to avoid the detection of the previous click of the Knob
+
+  position = knob.get();
+  click = knob.isPressed();
+  direction = knob.getDirection();
+
+  while(click == 0)
+  {
+    integrated_matrix.loadFrame(icon_MIN);
+    // Serial.println(minutes);
+    if (direction == 1 && minutes < 60){
+      buzzer.tone(330, duration);
+      minutes++;
+      currentTime = RTCTime(7, Month::APRIL, 2026, hours, minutes, 00, DayOfWeek::MONDAY, SaveLight::SAVING_TIME_ACTIVE);
+      RTC.setTime(currentTime);
+      matrix_show_time();
+    }
+    else if(direction == -1 && minutes > 0){
+      minutes--;
+      currentTime = RTCTime(7, Month::APRIL, 2026, hours, minutes, 00, DayOfWeek::MONDAY, SaveLight::SAVING_TIME_ACTIVE);
+      RTC.setTime(currentTime);
+      matrix_show_time();
+    }
+
+    position = knob.get();
+    click = knob.isPressed();
+    direction = knob.getDirection();
+  }
+
+  // Serial.println("Minutes Set");
+  buzzer.tone(frequency, duration);
+
+}
+
+
+
+
+
+//---------------------- DEBUGGING FUNCTIONS -----------------
+
+void numbers_demo_mode()
+{
+  // Numbers Demo Mode - Small Numbers
+  matrix_0.setFrame(NUM_0); 
+  matrix_1.setFrame(NUM_1);
+  matrix_2.setFrame(NUM_2);
+  matrix_3.setFrame(NUM_3);
+  delay(5000);
+
+  matrix_0.setFrame(NUM_4); 
+  matrix_1.setFrame(NUM_5);
+  matrix_2.setFrame(NUM_6);
+  matrix_3.setFrame(NUM_7);
+  delay(5000);
+
+  // Clear the display
+  matrix_0.clear();
+  matrix_1.clear();
+  matrix_2.clear();
+  matrix_3.clear();
+  delay(500);
+
+  matrix_0.setFrame(NUM_8); 
+  matrix_1.setFrame(NUM_9);
+  delay(5000);
+
+    // Clear the display
+  matrix_0.clear();
+  matrix_1.clear();
+  matrix_2.clear();
+  matrix_3.clear();
+  delay(500);
+
+  // Numbers Demo Mode
+  matrix_0.setFrame(BIG_NUM_0); 
+  matrix_1.setFrame(BIG_NUM_1);
+  matrix_2.setFrame(BIG_NUM_2);
+  matrix_3.setFrame(BIG_NUM_3);
+  delay(5000);
+
+  matrix_0.setFrame(BIG_NUM_4); 
+  matrix_1.setFrame(BIG_NUM_5);
+  matrix_2.setFrame(BIG_NUM_6);
+  matrix_3.setFrame(BIG_NUM_7);
+  delay(5000);
+
+  // Clear the display
+  matrix_0.clear();
+  matrix_1.clear();
+  matrix_2.clear();
+  matrix_3.clear();
+  delay(500);
+
+  matrix_0.setFrame(BIG_NUM_8); 
+  matrix_1.setFrame(BIG_NUM_9);
+  delay(5000);
+
+  //7 Segment Numbers
+  matrix_0.setFrame(SEG_NUM_0); 
+  matrix_1.setFrame(SEG_NUM_1);
+  matrix_2.setFrame(SEG_NUM_2);
+  matrix_3.setFrame(SEG_NUM_3);
+  delay(5000);
+
+  matrix_0.setFrame(SEG_NUM_4); 
+  matrix_1.setFrame(SEG_NUM_5);
+  matrix_2.setFrame(SEG_NUM_6);
+  matrix_3.setFrame(SEG_NUM_7);
+  delay(5000);
+
+  // Clear the display
+  matrix_0.clear();
+  matrix_1.clear();
+  matrix_2.clear();
+  matrix_3.clear();
+  delay(500);
+
+  matrix_0.setFrame(SEG_NUM_8); 
+  matrix_1.setFrame(SEG_NUM_9);
+  delay(5000);
+
+  // Clear the display
+  matrix_0.clear();
+  matrix_1.clear();
+  matrix_2.clear();
+  matrix_3.clear();
+  delay(500);
+}
+
+void dates_demo_mode()
+{
+  integrated_matrix.loadFrame(frame_MON);
+  delay(1000);
+
+  integrated_matrix.loadFrame(frame_TUE);
+  delay(1000);
+
+  integrated_matrix.loadFrame(frame_WED);
+  delay(1000);
+
+  integrated_matrix.loadFrame(frame_THU);
+  delay(1000);
+
+  integrated_matrix.loadFrame(frame_FRI);
+  delay(1000);
+
+  integrated_matrix.loadFrame(frame_SAT);
+  delay(1000);
+
+  integrated_matrix.loadFrame(frame_SUN);
+  delay(1000);
+}
+
+void debug_time()
+{
+  // Print out date (DD/MM//YYYY)
+  Serial.print(currentTime.getDayOfMonth());
+  Serial.print("/");
+  Serial.print(Month2int(currentTime.getMonth()));
+  Serial.print("/");
+  Serial.print(currentTime.getYear());
+  Serial.print(" - ");
+
+  // Print time (HH/MM/SS)
+  Serial.print(currentTime.getHour());
+  Serial.print(":");
+  Serial.print(currentTime.getMinutes());
+  Serial.print(":");
+  Serial.println(currentTime.getSeconds());  
+}
+
+
+
+
